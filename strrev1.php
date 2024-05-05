bdf865546cd3d9c5d4c40e577838b90a53a9507d
steven-halla <steven.r.halla@gmail.com>
2024-05-05 14:37:11 -0700

    fin up to screen shot 14

---
+++ strrev1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+echo "Hello world!";
+echo ("<br>");
+echo strrev("Hello world!");
+?>
+</body>
+</html>
+
---
+++ strrev2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+//echo "Hello world!";
+echo ("Steven Halla");
+echo ("<br>");
+
+echo ("CIS 243");
+echo ("<br>");
+
+echo ("PHP");
+
+
+echo ("<br>");
+echo ("<br>");
+echo strrev("Steven Halla!");
+echo ("<br>");
+
+echo strrev("CIS 243!");
+echo ("<br>");
+
+echo strrev("PHP!");
+?>
+</body>
+</html>
